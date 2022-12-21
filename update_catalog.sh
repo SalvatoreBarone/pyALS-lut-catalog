1d5e871519eb45634a8ced7df02f2397b10216ad
Salvatore Barone <salvator.barone@gmail.com>
2022-12-21 08:40:40 +0100

    update

--- update_catalog.sh
+++ update_catalog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sqlite3 lut_catalog.db .dump > lut_catalog.sql
-sqlite3 lut_catalog.db < lut_catalog.sql 
-git commit -a -m "update"
+# sqlite3 lut_catalog.db .dump > lut_catalog.sql
+# sqlite3 lut_catalog.db < lut_catalog.sql 
+# git commit -a -m "update"
 git pull
 sqlite3 lut_catalog.db < lut_catalog.sql 
 sqlite3 lut_catalog.db .dump > lut_catalog.sql
